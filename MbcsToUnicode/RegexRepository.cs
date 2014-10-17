6581909c75c144bd9cd2afc76e8750918e00c075
jcampos73 <jcampos73@vodafone.es>
2014-10-17 15:01:46 +0200

    RegexRepository fix

--- MbcsToUnicode/RegexRepository.cs
+++ MbcsToUnicode/RegexRepository.cs
@@ -12,16 +12,20 @@ namespace MbcsToUnicode
     {
         private class SearchPattern
         {
-            public SearchPattern(Regex a, Regex b, string r)
+            public SearchPattern(Regex a, Regex b, string r, string a_del, string b_del)
             {
                 begin = a;
                 end = b;
                 replace = r;
+                beginDelimiter = a_del;
+                endDelimiter = b_del;
             }
 
-            public Regex begin;
-            public Regex end;
-            public string replace;
+            public Regex begin;//Regex to search for
+            public Regex end;//Regex to find to check the ending
+            public string replace;//Replace expression
+            public string beginDelimiter;
+            public string endDelimiter;
         }
 
         private Dictionary<string, SearchPattern> _RegexDict;
@@ -32,7 +36,7 @@ namespace MbcsToUnicode
             {
                 //http://stackoverflow.com/questions/406230/regular-expression-to-match-string-not-containing-a-word
                 //{ " (\"", new Regex ("(([^_T])|(^((?!TRACE).)*))\\(\\\"")}//Example: ("
-                { " (\"", new SearchPattern(new Regex ("[^_TRACE]\\(\\\""), new Regex ("[^\\\\]\""), "_T(\"{0}\")")   }//Example: ("
+                { " (\"", new SearchPattern(new Regex ("[^_TRACE]\\(\\\""), new Regex ("[^\\\\]\""), "_T({0})", "\"", "\"")  }//Example: ("
             };
         }
 
@@ -60,10 +64,21 @@ namespace MbcsToUnicode
                             string match_value_end = entry.Value.end.Match(line, begin_pos + match_value.Length).Value;
                             int end_pos = entry.Value.end.Match(line, begin_pos + match_value.Length).Index;
 
+                            //Replaced part
+                            string a1 = match_value.Substring(0, match_value.IndexOf(entry.Value.beginDelimiter));
+                            string a2 = match_value.Substring(match_value.IndexOf(entry.Value.beginDelimiter), match_value.Length - match_value.IndexOf(entry.Value.beginDelimiter));
+                            string b1 = match_value_end.Substring(0, match_value_end.IndexOf(entry.Value.endDelimiter));
+                            string b2 = match_value_end.Substring(match_value_end.IndexOf(entry.Value.endDelimiter), match_value_end.Length - match_value_end.IndexOf(entry.Value.endDelimiter));
+
+                            string replaced_part = string.Format(entry.Value.replace, a2
+                                + line.Substring(begin_pos + match_value.Length, end_pos - begin_pos - match_value.Length)
+                                + b1 + b2
+                                );
+
                             //Replace line
-                            replaced_line = line.Substring(0, begin_pos)
-                                + string.Format(entry.Value.replace, line.Substring(begin_pos + match_value.Length, end_pos - begin_pos - match_value.Length))
-                                + line.Substring(end_pos, line.Length - end_pos);
+                            replaced_line = line.Substring(0, begin_pos) + a1
+                                + replaced_part
+                                + line.Substring(end_pos + match_value_end.Length, line.Length - end_pos - match_value_end.Length);
                         }
 
                         //Construct feedback
